9ae35f8819f3c13b221a5596eeb05509c258915f
Otto Peräkylä <otto@Ottos-MacBook-Pro.local>
2017-10-17 18:45:47 +0300

    form styling

--- src/components/Reply.js
+++ src/components/Reply.js
@@ -11,11 +11,36 @@ class Reply extends Component {
     }
     this.history = props.history
   }
+
+  showErrors(errors) {
+    console.log('showErrors', errors)
+  }
+
+  validate(data) {
+    let errors = {}
+
+    if ( !data.title ) {
+      errors.title = 'Please fill in title'
+    }
+
+    if ( !data.text ) {
+      errors.text = 'Please fill in text'
+    }
+    
+    return errors
+  } 
   
   sendPost(e) {
     e.preventDefault();
     const self = this;
     const data = serialize(e.target, { hash: true })
+    const errors = this.validate(data)
+
+    if ( Object.keys(errors).length > 0 ) {
+      this.showErrors(errors)
+      return false
+    }
+
     createPost(data).then((response) => {
       if (typeof data.id === 'undefined') {
         // This is not a reply but a new post. Go to post.
@@ -32,13 +57,16 @@ class Reply extends Component {
     return (
       <div className="container mt-5">
         <h4 style={styles.heading}>Snapchat your database</h4>
-        <form onSubmit={this.sendPost.bind(this)}>
+        <form className="form" onSubmit={this.sendPost.bind(this)}>
           { this.state.postID
             ? <input type="hidden" name="id" value={this.state.postID} />
-            : <input type="text" name="title" className="w-100" style={styles.input} />
+            : <div className="form-group"><label htmlFor="title">Title</label><input type="text" name="title" id="title" className="form-control w-100" style={styles.input} /></div>
           }
-          <textarea className="w-100" name="text" style={styles.textarea}></textarea><br />
-          <button type="submit">Snap</button>
+          <div className="form-group">
+            <label htmlFor="text">Text</label>
+            <textarea className="form-control w-100" id="text" name="text" style={styles.textarea}></textarea>
+          </div>
+          <button className="btn btn-primary" type="submit">Snap</button>
         </form>
       </div>
     )
@@ -51,7 +79,8 @@ const styles = {
   },
   textarea: {
     maxWidth: '500px',
-    height: '150px'
+    height: '150px',
+    resize: 'none'
   },
   input: {
     maxWidth: '500px'
